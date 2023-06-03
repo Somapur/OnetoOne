c52f3ce52fbe2125ae3a11894dd0983bdfa6dc5b
unknown <vanishripapu97@gmail.com>
2023-06-03 12:03:47 +0530

    my person commit

---
+++ Person.java
@@ -0,0 +1,57 @@
+package com.ty.onetoone;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Person 
+{	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int address;
+	private String gender;
+	private long phno;
+	@OneToOne
+	private PanCard card;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAddress() {
+		return address;
+	}
+	public void setAddress(int address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	public PanCard getCard() {
+		return card;
+	}
+	public void setCard(PanCard card) {
+		this.card = card;
+	}
+}
